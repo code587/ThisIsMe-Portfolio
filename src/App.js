c82dcba6ec33c73d70ceeec33fda683002050b1e
Stephanie <sandora375@outlook.com>
2022-01-28 02:28:57 -0600

    added contact info to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Section />
       <Portfolio />
+      <Contact />
     </main>
   );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,25 +2,30 @@ import React, { useState } from 'react';
 import coupledining from '../components/Images/coupledining.jpeg';
 import notepad from '../components/Images/notepad.jpeg';
 import companyteam from '../components/Images/companyteam.jpeg';
+import quiz from '../components/Images/quiz.jpeg';
 import '../styles/Portfolio.css';
 
 
 function Portfolio() {
     return (
     <div>
-    <h1>My Projects</h1>
-        <div className='project1'>
+    <h1>Portfolio</h1>
+        <div className='date'>
             <img src={coupledining} alt= 'couple dining'/>
             <a id='gitlink1' href='https://github.com/code587/Urban-Disco-Burrito'>
             </a>
         </div>
-        <div className='project2'>
+        <div className='notes'>
         <img src={notepad} alt= "note pad"/>
         <a id="gitlink2" href="https://github.com/code587/NoteforNoteTaker"></a>
         </div>
-        <div className='project3'>
+        <div className='workTeam'>
         <img src={companyteam} alt= "silouhette of work team"/>
-        <a id="gitlink2" href="https://github.com/code587/WorkSquad-ProfileGenerator"></a>
+        <a id="gitlink3" href="https://github.com/code587/WorkSquad-ProfileGenerator"></a>
+        </div>
+        <div className='quiz'>
+        <img src={quiz} alt= "supernatural actors"/>
+        <a id="gitlink4" href="https://github.com/code587/Quiz-supernatural-challenge"></a>
         </div>
     </div>
     )
